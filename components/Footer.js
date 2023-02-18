59446be8c2ca873c547eb26ee20576b0a90b8ebd
Šimon Káňa <simon.kana@seznam.cz>
2023-02-18 22:08:59 +0000

    Updated footer layout and styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,26 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Footer() {
-    const router = useRouter();
-    const { asPath: pathname } = router;
+
 
     return(
-        <div className="flex my-4 p-4 items-center justify-between bg-aulaGray rounded-md shadow-md shadow-gray-300">
+        <div className="block text-center md:flex mt-8 md:mt-12 -mb-8 md:mb-4 -mx-4 md:mx-0 py-4 px-4 md:px-0 items-center justify-between bg-aulaGray md:bg-transparent">
            <div>
-        <Link href="/donate" className="block py-1 px-2 text-sm font-semibold text-white bg-aulaBlack rounded-md hover:bg-indigo-600">
+        <Link href="/donate" className="block mx-auto mt-2 mb-4 md:mt-0 md:mb-0 w-20 md:w-full py-1 px-2 text-sm font-semibold text-white bg-aulaBlack rounded-md hover:bg-indigo-600">
            DONATE
             </Link>
            </div>
-            <div className="flex">
+            <div className="block md:flex md:gap-2">
             <Link href="/privacy-policy"
-                className={
-                pathname === "/privacy-policy" ? "ml-2 text-sm font-semibold text-aulaBlack" : "ml-2 text-sm font-semibold text-white"
-                }>Privacy Policy</Link>
+                className="text-sm text-aulaBlack"
+                >Privacy Policy</Link>
+                <br className="block md:hidden" />
             <Link href="/terms-and-conditions"
-                className={
-                pathname === "/terms-and-conditions" ? "ml-2 text-sm font-semibold text-aulaBlack" : "ml-2 text-sm font-semibold text-white"
-                }>Terms and Conditions</Link>
-                           <span className="ml-8 text-sm text-white">© 2023 NFTaula.io</span>
+                className="text-sm text-aulaBlack"
+                >
+                    Terms and Conditions</Link>
+                    <p className="mt-4 mb-2 md:mt-0 md:mb-0 text-sm text-aulaBlack">© 2023 NFTaula.io</p>
+                           
             </div>
       </div>
     );
