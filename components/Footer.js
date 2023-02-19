84c65ff26e79ec95f32379ce933fc59e607f9e96
Šimon Káňa <simon.kana@seznam.cz>
2023-02-19 00:42:54 +0000

    Fixed footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
 
     return(
-        <div className="block text-center md:flex mt-8 md:mt-12 -mb-8 md:mb-4 -mx-4 md:mx-0 py-4 px-4 md:px-0 items-center justify-between bg-aulaGray md:bg-transparent">
+        <div className="block text-center md:flex mt-8 md:mt-2 -mb-8 md:mb-4 -mx-4 md:mx-0 py-4 px-4 md:px-0 items-center justify-between bg-aulaGray md:bg-transparent">
            <div>
         <Link href="/donate" className="block mx-auto mt-2 mb-4 md:mt-0 md:mb-0 w-20 md:w-full py-1 px-2 text-sm font-semibold text-white bg-aulaBlack rounded-md hover:bg-indigo-600">
            DONATE
