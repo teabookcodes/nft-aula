65d3ddb42ed0418e5b8b806bdfde7e2600603f1c
Šimon Káňa <simon.kana@seznam.cz>
2023-02-25 23:10:28 +0000

    Fixed build error

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ export default function PrivacyPolicyPage() {
                     <p className="block mb-2">I am a passionate NFT collector and creator, known on Twitter as <Link className="font-semibold text-aulaBlack" href="https://twitter.com/NFTbagari" target="_blank">@NFTbagari</Link>.</p>
                     <p className="block mb-2">I have been creating NFTs and I know firsthand how difficult it is not only to create them, but even more difficult to sell them.</p>
                     <p className="block mb-4">However, as a collector myself, I understand the thrill and excitement that comes with discovering new NFTs to add to my collection.</p>
-                    <p className="block mb-2">That's why I created NFTaula.io - <strong>to provide a platform for NFT creators to showcase their work and for collectors like myself to easily browse and discover new pieces to add to our collections.</strong>
+                    <p className="block mb-2">That&apos;s why I created NFTaula.io - <strong>to provide a platform for NFT creators to showcase their work and for collectors like myself to easily browse and discover new pieces to add to our collections.</strong>
                     </p>
                     <p className="block mb-2">I believe that NFTs have the power to revolutionize the way we think about digital ownership and art.</p>
                     <p className="block mb-2">They offer a unique opportunity for artists to showcase their work in a new and exciting way, and for collectors to own truly one-of-a-kind pieces that can hold value over time.</p>
