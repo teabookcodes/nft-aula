8fd860b3d6c472ca7d6bec3e35ae56781b065a58
Šimon Káňa <simon.kana@seznam.cz>
2023-03-03 17:21:50 +0000

    Updated about page structure and styling

--- pages/about.js
+++ pages/about.js
@@ -5,27 +5,59 @@ import Link from "next/link";
 export default function PrivacyPolicyPage() {
     return (
         <Layout>
-            <Card>
-                <div className="text-center md:text-left">
-                    <h1 className="text-xl font-semibold text-aulaBlack">About</h1>
-
-                    <h2>THE STORY OF ME AND NFTAULA.IO</h2>
-                    <br />
-                    <img className="rounded-md mx-auto md:mx-0 mb-4" src="https://pbs.twimg.com/profile_images/1622072686284312576/IahrC2Ed_400x400.jpg" alt="NFTbagari" />
-                    <p className="block mb-2">I am a passionate NFT collector and creator, known on Twitter as <Link className="font-semibold text-aulaBlack" href="https://twitter.com/NFTbagari" target="_blank">@NFTbagari</Link>.</p>
-                    <p className="block mb-2">I have been creating NFTs and I know firsthand how difficult it is not only to create them, but even more difficult to sell them.</p>
-                    <p className="block mb-4">However, as a collector myself, I understand the thrill and excitement that comes with discovering new NFTs to add to my collection.</p>
-                    <p className="block mb-2">That&apos;s why I created NFTaula.io - <strong>to provide a platform for NFT creators to showcase their work and for collectors like myself to easily browse and discover new pieces to add to our collections.</strong>
+            {/* <Card> */}
+            <div className="mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl text-center md:text-left font-bold text-aulaBlack">
+                    About
+                </h1>
+                <p className="mt-4 text-gray-700">
+                    Welcome to NFTaula.io, a platform for NFT creators and collectors! My
+                    name is Ladislav Bagari, also known as NFTbagari on Twitter. I am a
+                    passionate NFT collector and creator, and I created NFTaula.io to
+                    provide a space where creators can showcase their work and collectors
+                    can easily browse and discover new pieces to add to their collections.
+                </p>
+                <div className="my-8">
+                    <img
+                        className="rounded-md mx-auto mb-4"
+                        src="https://pbs.twimg.com/profile_images/1622072686284312576/IahrC2Ed_400x400.jpg"
+                        alt="NFTbagari"
+                    />
+                    <p className="text-center text-gray-500">
+                        Ladislav Bagari, creator of NFTaula.io
                     </p>
-                    <p className="block mb-2">I believe that NFTs have the power to revolutionize the way we think about digital ownership and art.</p>
-                    <p className="block mb-2">They offer a unique opportunity for artists to showcase their work in a new and exciting way, and for collectors to own truly one-of-a-kind pieces that can hold value over time.</p>
-                    <p className="block mb-4">Every time I add a new NFT to my collection, I feel a rush of excitement and satisfaction knowing that I own a piece of digital history. I am constantly inspired by the creativity and innovation that I see in the NFT space, and I am thrilled to be a part of this rapidly growing community.</p>
-                    <p className="block mb-4">Have a nice day!</p>
-                    <p className="block">With respect</p>
-                    <p className="block mb-2 font-semibold">Ladislav Bagari</p>
-
                 </div>
-            </Card>
+                <h2 className="text-xl text-center md:text-left font-bold text-aulaBlack">
+                    My Story
+                </h2>
+                <p className="mt-4 text-gray-700">
+                    As an NFT collector myself, I understand the thrill and excitement of
+                    discovering new pieces to add to my collection. However, I also know
+                    how difficult it can be for NFT creators to sell their work. That's
+                    why I created NFTaula.io - to provide a platform where creators can
+                    showcase their work and collectors can easily find and purchase unique
+                    pieces.
+                </p>
+                <p className="mt-4 text-gray-700">
+                    I believe that NFTs have the power to revolutionize the way we think
+                    about digital ownership and art. They offer a new and exciting way for
+                    artists to showcase their work and for collectors to own one-of-a-kind
+                    pieces that can hold value over time.
+                </p>
+                <p className="mt-4 text-gray-700">
+                    Every time I add a new NFT to my collection, I feel a rush of
+                    excitement and satisfaction knowing that I own a piece of digital
+                    history. I am constantly inspired by the creativity and innovation in
+                    the NFT space, and I am thrilled to be a part of this rapidly growing
+                    community.
+                </p>
+                <p className="mt-8 text-gray-700">
+                    Thank you for visiting NFTaula.io. Have a nice day!
+                </p>
+                <p className="mt-8 text-gray-700">With respect,</p>
+                <p className="font-bold text-aulaGray">Ladislav Bagari</p>
+            </div>
+            {/* </Card> */}
         </Layout>
-    )
+    );
 }
