17534a9e8a935fa6aa95c35c6935c9c328967cef
Šimon Káňa <simon.kana@seznam.cz>
2023-03-10 11:53:41 +0000

    Updated tables in Supabase db, Delete and Save functions

--- components/NftCard.js
+++ components/NftCard.js
@@ -3,7 +3,7 @@ import ClickOutHandler from "react-clickout-handler";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import Image from "next/image";
 
 export default function NftCard({
@@ -37,18 +37,18 @@ export default function NftCard({
   useEffect(() => {
     if (session?.user?.id) {
       const fetchData = () => {
-        // supabase
-        //   .from("saved_nfts")
-        //   .select()
-        //   .eq("nft_id", id)
-        //   .eq("user_id", session.user.id)
-        //   .then((result) => {
-        //     if (result.data && result.data.length > 0) {
-        //       setIsSaved(true);
-        //     } else {
-        //       setIsSaved(false);
-        //     }
-        //   });
+        supabase
+          .from("saved_nfts")
+          .select()
+          .eq("nft_id", id)
+          .eq("user_id", session.user.id)
+          .then((result) => {
+            if (result.data && result.data.length > 0) {
+              setIsSaved(true);
+            } else {
+              setIsSaved(false);
+            }
+          });
 
         supabase
           .from("nfts")
@@ -80,73 +80,53 @@ export default function NftCard({
     setDropdownOpen(false);
   }
 
-  // function toggleSave() {
-  //   if (isSaved) {
-  //     supabase
-  //       .from("saved_nfts")
-  //       .delete()
-  //       .eq("nft_id", id)
-  //       .eq("user_id", session.user.id)
-  //       .then((result) => {
-  //         setIsSaved(false);
-  //         setDropdownOpen(false);
-  //       });
-  //   }
-  //   if (!isSaved) {
-  //     supabase
-  //       .from("saved_nfts")
-  //       .insert({
-  //         user_id: session.user.id,
-  //         nft_id: id,
-  //       })
-  //       .then((result) => {
-  //         setIsSaved(true);
-  //         setDropdownOpen(false);
-  //       });
-  //   }
-  // }
-
-  function toggleEdit() {
-    return;
+  function toggleSave() {
+    if (isSaved) {
+      supabase
+        .from("saved_nfts")
+        .delete()
+        .eq("nft_id", id)
+        .eq("user_id", session.user.id)
+        .then((result) => {
+          setIsSaved(false);
+          setDropdownOpen(false);
+          alert("NFT unsaved successfully!");
+          window.location.reload();
+        });
+    }
+    if (!isSaved) {
+      supabase
+        .from("saved_nfts")
+        .insert({
+          user_id: session.user.id,
+          nft_id: id,
+        })
+        .then((result) => {
+          setIsSaved(true);
+          setDropdownOpen(false);
+          alert("NFT saved successfully!");
+        });
+    }
   }
 
-  // async function toggleDelete() {
-  //   if (isSaved) {
-  //     supabase
-  //       .from("saved_nfts")
-  //       .delete()
-  //       .eq("nft_id", id)
-  //       .eq("user_id", session.user.id)
-  //       .then((result) => {
-  //         setIsSaved(false);
-  //         setDropdownOpen(false);
-  //       });
-  //   }
-  //   if (canDelete) {
-  //     const { error } = await supabase.from("nfts").delete().eq("id", id);
-  //   }
-  // }
+  function toggleEdit() {
+    alert("NFT successfully edited!");
+  }
 
   async function toggleDelete() {
     if (canDelete) {
       const { error } = await supabase.from("nfts").delete().eq("id", id);
     }
     setDropdownOpen(false);
+    alert("NFT successfully deleted!");
+    window.location.reload();
   }
 
-  // ALERT BN!VXJ HCBXJm!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  // async function unsaveNft() {
-  //   await supabase
-  //     .from("saved_nfts")
-  //     .delete("*")
-  //     .filter("nft_id", "eq", id)
-  //     .then((result) => {
-  //       setIsSaved(false);
-  //     });
-  // }
-
   return (
-    <Card nftCard={pathname === "/saved" || pathname === "/profile" ? false : true} marginBottom={pathname === "/saved" ? true : false}>
+    <Card
+      nftCard={pathname === "/saved" || pathname === "/profile" ? false : true}
+      marginBottom={pathname === "/saved" ? true : false}
+    >
       <div className="flex justify-between">
         <div>
           {/* <Link href="/nft-detail"> */}
@@ -159,16 +139,13 @@ export default function NftCard({
           </h3>
           {/* <p className="text-sm mb-2 text-gray-500">{description}</p> */}
           <h3 className="text-base">
-            Category:{" "}
-            <span className="font-semibold">{category}</span>
+            Category: <span className="font-semibold">{category}</span>
           </h3>
           <h3 className="text-base">
-            Marketplace:{" "}
-            <span className="font-semibold">{marketplace}</span>
+            Marketplace: <span className="font-semibold">{marketplace}</span>
           </h3>
           <h3 className="text-base">
-            Blockchain:{" "}
-            <span className="font-semibold">{blockchain}</span>
+            Blockchain: <span className="font-semibold">{blockchain}</span>
           </h3>
           <h3 className="text-lg my-2">
             Price:{" "}
@@ -177,7 +154,8 @@ export default function NftCard({
             </span>
           </h3>
         </div>
-        {session && author == session?.user?.id && (
+        {/* {session && author == session?.user?.id && ( */}
+        {session && (
           <div className="relative">
             <button className="text-gray-400" onClick={openDropdown}>
               <svg
@@ -203,7 +181,7 @@ export default function NftCard({
               <div className="relative">
                 {dropdownOpen && (
                   <div className="absolute -right-6 bg-white dark:bg-gray-800 shadow-md shadow-gray-300 dark:shadow-none p-3 rounded-md border border-gray-100 dark:border-gray-900 w-52">
-                    {/* <button onClick={toggleSave} className="w-full">
+                    <button onClick={toggleSave} className="w-full">
                       <span className="flex gap-3 py-2 hover:bg-aulaBlack hover:text-white px-4 -mx-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300">
                         {isSaved && (
                           <svg
@@ -241,7 +219,7 @@ export default function NftCard({
 
                         {isSaved ? "Unsave" : "Save NFT"}
                       </span>
-                    </button> */}
+                    </button>
                     {/* <a
                             href=""
                             className="flex gap-3 py-2 my-2 hover:bg-aulaBlack hover:text-white -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300"
@@ -334,11 +312,7 @@ export default function NftCard({
       </div>
 
       <div className="rounded-md overflow-hidden mt-2 mb-4 w-full h-60 flex items-center">
-        <img
-          src={nftImage}
-          alt={nftName}
-          className="w-full"
-        />
+        <img src={nftImage} alt={nftName} className="w-full" />
       </div>
 
       {/* {nftImage?.length > 0 && (
@@ -360,8 +334,19 @@ export default function NftCard({
               className="font-semibold"
               title="Website"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                />
               </svg>
             </a>
           </p>
@@ -375,7 +360,23 @@ export default function NftCard({
               className="font-semibold"
               title="Twitter"
             >
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="currentColor"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M11.919 24.94c-2.548 0-4.921-.747-6.919-2.032a9.049 9.049 0 0 0 6.681-1.867 4.512 4.512 0 0 1-4.215-3.137c.276.054.559.082.848.082.412 0 .812-.056 1.193-.156a4.519 4.519 0 0 1-3.622-4.425v-.059a4.478 4.478 0 0 0 2.042.564 4.507 4.507 0 0 1-2.008-3.758c0-.824.225-1.602.612-2.268a12.811 12.811 0 0 0 9.303 4.715 4.517 4.517 0 0 1 7.692-4.115 9.107 9.107 0 0 0 2.866-1.094 4.542 4.542 0 0 1-1.983 2.498 9.08 9.08 0 0 0 2.592-.71 9.283 9.283 0 0 1-2.252 2.337c.008.193.014.388.014.583-.001 5.962-4.542 12.843-12.844 12.842"></path></g></svg>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path d="M11.919 24.94c-2.548 0-4.921-.747-6.919-2.032a9.049 9.049 0 0 0 6.681-1.867 4.512 4.512 0 0 1-4.215-3.137c.276.054.559.082.848.082.412 0 .812-.056 1.193-.156a4.519 4.519 0 0 1-3.622-4.425v-.059a4.478 4.478 0 0 0 2.042.564 4.507 4.507 0 0 1-2.008-3.758c0-.824.225-1.602.612-2.268a12.811 12.811 0 0 0 9.303 4.715 4.517 4.517 0 0 1 7.692-4.115 9.107 9.107 0 0 0 2.866-1.094 4.542 4.542 0 0 1-1.983 2.498 9.08 9.08 0 0 0 2.592-.71 9.283 9.283 0 0 1-2.252 2.337c.008.193.014.388.014.583-.001 5.962-4.542 12.843-12.844 12.842"></path>
+                </g>
+              </svg>
             </a>
           </p>
         )}
@@ -387,8 +388,19 @@ export default function NftCard({
             className="font-semibold"
             title="Open collection on Marketplace"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+              />
             </svg>
           </a>
         </p>
@@ -400,20 +412,28 @@ export default function NftCard({
             className="font-semibold"
             title="Buy NFT on Marketplace"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+              />
             </svg>
-
           </a>
         </p>
       </div>
-      {
-        pathname === "/saved" && (
-          <p className="text-gray-400 text-sm">
-            NFT was uploaded by: <span className="cursor-pointer">{author}</span>
-          </p>
-        )
-      }
-    </Card >
+      {pathname === "/saved" && (
+        <p className="text-gray-400 text-sm">
+          NFT was uploaded by: <span className="cursor-pointer">{author}</span>
+        </p>
+      )}
+    </Card>
   );
 }
--- pages/saved.js
+++ pages/saved.js
@@ -9,30 +9,30 @@ export default function SavedNftsPage() {
   const supabase = useSupabaseClient();
   const session = useSession();
 
-  // useEffect(() => {
-  //   if (!session?.user?.id) {
-  //     return;
-  //   }
-  //   supabase
-  //     .from("saved_nfts")
-  //     .select("nft_id")
-  //     .eq("user_id", session.user.id)
-  //     .then((result) => {
-  //       const nftsIds = result.data.map((item) => item.nft_id);
-  //       supabase
-  //         .from("nfts")
-  //         .select("*")
-  //         .in("id", nftsIds)
-  //         .order("created_at", { ascending: false })
-  //         .then((result) => setNfts(result.data));
-  //     });
-  // }, [supabase, session?.user?.id]);
+  useEffect(() => {
+    if (!session?.user?.id) {
+      return;
+    }
+    supabase
+      .from("saved_nfts")
+      .select("nft_id")
+      .eq("user_id", session.user.id)
+      .then((result) => {
+        const nftsIds = result.data.map((item) => item.nft_id);
+        supabase
+          .from("nfts")
+          .select("*")
+          .in("id", nftsIds)
+          .order("created_at", { ascending: false })
+          .then((result) => setNfts(result.data));
+      });
+  }, [supabase, session?.user?.id]);
 
   return (
     <Layout>
-      {/* <h1 className="text-4xl mb-4 text-gray-800 text-center md:text-left">
-        Saved NFTs
-      </h1> */}
+      <h1 className="text-2xl mb-4 text-aulaBlack font-semibold text-center md:text-left">
+        Saved NFTs:
+      </h1>
       {!session && (
         <Card>
           <p>You must login first to view your saved NFTs.</p>
