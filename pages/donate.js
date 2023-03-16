623351aad43bbc3ee48f545a073ca3da9f21c8d1
Šimon Káňa <simon.kana@seznam.cz>
2023-03-16 21:45:33 +0000

    Fixed the light mode styling for Donate page

--- pages/donate.js
+++ pages/donate.js
@@ -96,7 +96,7 @@ export default function DonatePage() {
           <p className="mx-auto md:ml-28 font-semibold text-aulaBlack">BTC</p>
 
           <Image
-            className="mx-auto md:mx-0 my-4 rounded-md"
+            className="mx-auto md:mx-0 my-4 rounded-md shadow-xl shadow-gray-200 border-2 border-gray-200 dark:shadow-none"
             src="/BTC.png"
             alt="BTC QRcode"
             width="240"
@@ -104,8 +104,8 @@ export default function DonatePage() {
           />
 
           <div className="flex gap-2 mx-auto md:mx-0 max-w-[240px]">
-            <p className="mt-2 bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{btcWalletAddress}</p>
-            <button className="mt-2 bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyBtcAddress}>
+            <p className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{btcWalletAddress}</p>
+            <button className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyBtcAddress}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
               </svg>
@@ -116,7 +116,7 @@ export default function DonatePage() {
           <p className="mx-auto md:ml-28 font-semibold text-aulaBlack">LTC</p>
 
           <Image
-            className="mx-auto md:mx-0 my-4 rounded-md"
+            className="mx-auto md:mx-0 my-4 rounded-md shadow-xl shadow-gray-200 border-2 border-gray-200 dark:shadow-none"
             src="/LTC.png"
             alt="LTC QRcode"
             width="240"
@@ -124,8 +124,8 @@ export default function DonatePage() {
           />
 
           <div className="flex gap-2 mx-auto md:mx-0 max-w-[240px]">
-            <p className="mt-2 bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{ltcWalletAddress}</p>
-            <button className="mt-2 bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyLtcAddress}>
+            <p className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{ltcWalletAddress}</p>
+            <button className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyLtcAddress}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
               </svg>
@@ -136,7 +136,7 @@ export default function DonatePage() {
           <p className="mx-auto mt:0 md:-mt-6 md:ml-6 font-semibold text-center text-aulaBlack">ETH<br />(and all ERC-20 tokens)</p>
 
           <Image
-            className="mx-auto md:mx-0 my-4 rounded-md"
+            className="mx-auto md:mx-0 my-4 rounded-md shadow-xl shadow-gray-200 border-2 border-gray-200 dark:shadow-none"
             src="/ETH.png"
             alt="ETH QRcode"
             width="240"
@@ -144,8 +144,8 @@ export default function DonatePage() {
           />
 
           <div className="flex gap-2 mx-auto md:mx-0 max-w-[240px]">
-            <p className="mt-2 bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{ethWalletAddress}</p>
-            <button className="mt-2 bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyEthAddress}>
+            <p className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{ethWalletAddress}</p>
+            <button className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyEthAddress}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
               </svg>
@@ -156,7 +156,7 @@ export default function DonatePage() {
           <p className="mx-auto md:ml-24 font-semibold text-aulaBlack">DOGE</p>
 
           <Image
-            className="mx-auto md:mx-0 my-4 rounded-md"
+            className="mx-auto md:mx-0 my-4 rounded-md shadow-xl shadow-gray-200 border-2 border-gray-200 dark:shadow-none"
             src="/DOGE.png"
             alt="DOGE QRcode"
             width="240"
@@ -164,8 +164,8 @@ export default function DonatePage() {
           />
 
           <div className="flex gap-2 mx-auto md:mx-0 max-w-[240px]">
-            <p className="mt-2 bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{dogeWalletAddress}</p>
-            <button className="mt-2 bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyDogeAddress}>
+            <p className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md overflow-hidden">{dogeWalletAddress}</p>
+            <button className="mt-2 bg-gray-200 dark:bg-gray-600 px-1 py-1 rounded-md w-8 max-w-[240px]" onClick={handleCopyDogeAddress}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
               </svg>
@@ -179,7 +179,7 @@ export default function DonatePage() {
       </h3>
       <div className="flex flex-col mt-4">
         <Image
-          className="mx-auto mb-4 rounded-md"
+          className="mx-auto mb-4 rounded-md shadow-xl shadow-gray-200 border-2 border-gray-200 dark:shadow-none"
           src="/PayPal_QR.png"
           alt="PayPal QRcode"
           width="240"
@@ -187,12 +187,12 @@ export default function DonatePage() {
         />
 
         <Link
-          className="flex gap-2 items-center px-3 py-1 mx-auto rounded-md bg-gray-700"
+          className="flex gap-2 items-center px-3 py-1 mx-auto rounded-md bg-gray-200 dark:bg-gray-600"
           href="https://www.paypal.com/paypalme/NFTaula"
           target="_blank"
         >
           paypal.com
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
           </svg>
 
