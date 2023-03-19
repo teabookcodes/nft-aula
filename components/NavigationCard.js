350160f2546ff7929c802865b14d10067c062447
Šimon Káňa <simon.kana@seznam.cz>
2023-03-19 12:20:10 +0000

    Changed icon for List NFT page

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -86,20 +86,10 @@ export default function NavigationCard() {
                     : nonActiveElementClasses
                 }
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
+
                 <span className="hidden">List new NFT</span>
               </Link>
               <Link
