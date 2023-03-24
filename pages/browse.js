8905164cdbe4a5752a2eb140e561d1b20b2836b6
Šimon Káňa <simon.kana@seznam.cz>
2023-03-24 13:12:34 +0000

    Fix

--- pages/browse.js
+++ pages/browse.js
@@ -309,7 +309,7 @@ export default function BrowsePage() {
           </div>
         </Card>
       )}
-      <div className="mt-12 md:mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="mt-12 md:mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {isLoading ? ( // Render loading indicator while data is being fetched
           <div>Loading NFTs...</div>
         ) : (
