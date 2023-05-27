aa3f3d38d1925ab7776afed1aa34f2847231bc59
teabookcodes <simon.kana@seznam.cz>
2023-05-27 18:22:57 +0200

    Fixed pagination styling

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,25 +1,38 @@
 import React from "react";
 
-export default function Pagination({ totalNfts, nftsPerPage, currentPage, setCurrentPage }) {
-    const activeButton = "text-gray-50 bg-indigo-700 w-10 mr-2 px-3 py-1 rounded-md"
-    const nonActiveButton = "text-gray-50 bg-gray-800 w-10 mr-2 px-3 py-1 rounded-md hover:bg-indigo-600 my-1"
+export default function Pagination({
+  totalNfts,
+  nftsPerPage,
+  currentPage,
+  setCurrentPage,
+}) {
+  const activeButton =
+    "text-gray-50 bg-indigo-700 w-10 mr-2 px-3 py-1 rounded-md";
+  const nonActiveButton =
+    "dark:text-gray-50 bg-gray-200 dark:bg-gray-800 w-10 mr-2 px-3 py-1 rounded-md hover:text-gray-50 hover:bg-indigo-600 dark:hover:bg-indigo-600 my-1";
 
-    let pages = [];
+  let pages = [];
 
-    for (let i = 1; i <= Math.ceil(totalNfts / nftsPerPage); i++) {
-        pages.push(i)
-    }
+  for (let i = 1; i <= Math.ceil(totalNfts / nftsPerPage); i++) {
+    pages.push(i);
+  }
 
-    return (
-        <div className="text-center md:text-left">
-            {pages.map((page, index) => {
-                return <button key={index} onClick={() => {
-                    setCurrentPage(page);
-                    window.scrollTo(0, 0);
-                }} className={page === currentPage ? activeButton : nonActiveButton}>
-                    {page}
-                </button>
-            })}
-        </div>
-    )
+  return (
+    <div className="text-center md:text-left">
+      {pages.map((page, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => {
+              setCurrentPage(page);
+              window.scrollTo(0, 0);
+            }}
+            className={page === currentPage ? activeButton : nonActiveButton}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
