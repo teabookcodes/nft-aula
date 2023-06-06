e702efdecc67265f032ded5bc9443065197f89ff
teabookcodes <simon.kana@seznam.cz>
2023-06-06 11:40:37 +0200

    Fixed the link displaying conditions

--- components/NftCard.js
+++ components/NftCard.js
@@ -791,54 +791,58 @@ export default function NftCard({
               </a>
             </p>
           )}
-          <p className="block p-1 rounded-full bg-white text-aulaBlack hover:text-indigo-600">
-            <a
-              href={collectionLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-semibold"
-              title="Open collection on Marketplace"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+          {collectionLink !== "" && (
+            <p className="block p-1 rounded-full bg-white text-aulaBlack hover:text-indigo-600">
+              <a
+                href={collectionLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-semibold"
+                title="Open collection on Marketplace"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
-                />
-              </svg>
-            </a>
-          </p>
-          <p className="block p-1 text-left rounded-full bg-white text-aulaBlack hover:text-indigo-600">
-            <a
-              href={marketplaceLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-semibold"
-              title="Buy NFT on Marketplace"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+                  />
+                </svg>
+              </a>
+            </p>
+          )}
+          {marketplaceLink !== "" && (
+            <p className="block p-1 text-left rounded-full bg-white text-aulaBlack hover:text-indigo-600">
+              <a
+                href={marketplaceLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-semibold"
+                title="Buy NFT on Marketplace"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
-                />
-              </svg>
-            </a>
-          </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+                  />
+                </svg>
+              </a>
+            </p>
+          )}
         </div>
         {/* {pathname === "/saved" && (
           <p className="text-gray-400 text-sm">
