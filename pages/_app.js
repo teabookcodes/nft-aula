a34ee37c10ef7506a889cd2e1280b736f6e55c68
teabookcodes <simon.kana@seznam.cz>
2023-10-22 23:26:37 +0200

    Added maintenance page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes"
+import MaintenancePage from "@/components/MaintenancePage";
+import { ThemeProvider } from "next-themes";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [maintenance, setMaintenance] = useState(true);
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  if (maintenance) {
+    return <MaintenancePage />;
+  }
 
   return (
     <SessionContextProvider
