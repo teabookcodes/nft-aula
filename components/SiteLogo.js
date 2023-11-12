3b088d1a96e529d125fe7b12cecc4e28fdf36d30
teabookcodes <simon.kana@seznam.cz>
2023-11-12 12:44:46 +0100

    Fixed the site logo redirect

--- components/SiteLogo.js
+++ components/SiteLogo.js
@@ -3,12 +3,7 @@ import Image from "next/image";
 
 export default function SiteLogo() {
   return (
-    <Link
-      href="https://www.nftaula.io/"
-      onClick={() => {
-        window.location.reload();
-      }}
-    >
+    <Link href="https://www.nftaula.io/">
       <Image src="logo.svg" width="180" height="180" alt="NFTaula logo" />
     </Link>
   );
